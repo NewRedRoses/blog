f77471688897c9f089b893e5e40c16693a99cf2c
Mark A. <mark001616@gmail.com>
2024-11-23 18:19:59 -0500

    Add support for JSON and form data

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import "dotenv/config";
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/posts", posts);
 
 app.listen(port, () => {
