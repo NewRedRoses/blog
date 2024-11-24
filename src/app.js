1c943781b8c362e954ab4ff2c31a03d9a8a48310
Mark A. <mark001616@gmail.com>
2024-11-23 22:23:47 -0500

    Add cors as dependency and apply it

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { posts } from "./Routers/postsRouter.js";
 import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 const port = process.env.port || 3000;
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
