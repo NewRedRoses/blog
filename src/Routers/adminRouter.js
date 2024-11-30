567ada5216f83533a5b1ac4d9b176200063d8438
Mark A. <mark001616@gmail.com>
2024-11-29 20:22:35 -0500

    Add route to get all posts

--- src/Routers/adminRouter.js
+++ src/Routers/adminRouter.js
@@ -1,6 +1,18 @@
 import { Router } from "express";
+import { verifyToken } from "../app.js";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 export const admin = Router();
 
-admin.get("/posts", (req, res) => {
-  res.json({ msg: "get all posts" });
+admin.get("/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+    if (err) {
+      res.json(403);
+    } else {
+      const posts = await prisma.post.findMany({});
+      res.json(posts);
+    }
+  });
 });
