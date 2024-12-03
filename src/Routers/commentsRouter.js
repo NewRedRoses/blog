8b3886fb131b23a3372558aaadbb3e029799d7e4
Mark A. <mark001616@gmail.com>
2024-12-02 22:01:57 -0500

    Display only relevant  and sort comments by descending order

--- src/Routers/commentsRouter.js
+++ src/Routers/commentsRouter.js
@@ -4,10 +4,23 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export const comments = Router();
 
-comments.get("", async (req, res) => {
+comments.get("/:postId", async (req, res) => {
   const comments = await prisma.postComments.findMany({
     where: {
       postId: parseInt(req.params.postId),
+    },
+    orderBy: {
+      date_created: "desc",
+    },
+    select: {
+      id: true,
+      text: true,
+      date_created: true,
+      user: {
+        select: {
+          name: true,
+        },
+      },
     },
   });
   res.json(comments);
